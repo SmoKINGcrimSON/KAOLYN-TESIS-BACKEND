03b4716add06a47780dcf0dad3639e5d1385de0d
smoking <ferchowarrior169@gmail.com>
2025-06-09 18:56:40 -0500

    deploy 0.0.4

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -5,6 +5,6 @@ const User = z.object({
   password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/)
 });
 
-export function validateUser({user}){
+export function validateUser(user){
     return User.safeParse(user)
 }
