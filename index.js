65ada55ded0344f9147ef3fc1df5e4fc778a8978
smoking <ferchowarrior169@gmail.com>
2025-06-09 18:20:00 -0500

    ready for deplayment

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import { userRouter } from './router/userRouter.js';
 
-export const app = express()
-const hostname = '192.168.1.170'
-const port = 3000
+//export const app = express()
+//const hostname = '192.168.1.170'
+//const port = 3000
 
+const app = express()
 app.use(express.json());
-
 app.use('/user', userRouter)
 
+export default app
+/*
 app.listen(port, () => {
   console.log(`server is listening on: http://${hostname}:${port}`)
-})
+})*/
